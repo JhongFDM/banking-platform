ad19ff08255f98bb72c7a82ccff81c3cc3c8c98b
Sai Bharath Reddy Chodavaram <bharathreddybannu141@gmail.com>
2026-07-03 12:48:32 -0400

    fixing tests

--- VoltioAngularFrontend/src/app/Components/nav-bar/nav-bar.spec.ts
+++ VoltioAngularFrontend/src/app/Components/nav-bar/nav-bar.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavBar } from './nav-bar';
+import { provideRouter } from '@angular/router';
 
 describe('NavBar', () => {
   let component: NavBar;
@@ -9,6 +10,7 @@ describe('NavBar', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NavBar],
+      providers: [provideRouter([])]
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavBar);
