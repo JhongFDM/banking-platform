f5b2209bbab63cadc0e444233020d792299696c1
Sofia Rahul <sofiarahul7@gmail.com>
2026-07-08 14:40:27 -0400

    testing

--- VoltioAngularFrontend/src/app/Components/my-accounts/my-accounts.ts
+++ VoltioAngularFrontend/src/app/Components/my-accounts/my-accounts.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './my-accounts.html',
   styleUrl: './my-accounts.css',
 })
-export class MyAccounts {}
+export class MyAccounts {
+
+  //get current customer id
+
+  //Connect to the accounts service to get all accounts for customer with id
+
+  //ngOnInit, display a card with interior cards of each of the accounts that belongs to the customer
+
+  
+
+}
