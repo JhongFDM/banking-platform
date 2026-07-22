53c033b6530fc33b64b6785ba4d4f121a4c56157
bit-yottabyte <60991230+bit-yottabyte@users.noreply.github.com>
2026-07-22 12:51:43 -0400

    migrated customer-profile, customer-detail, customer-edit, and customer-create

--- VoltioAngularFrontend/src/app/app.routes.ts
+++ VoltioAngularFrontend/src/app/app.routes.ts
@@ -2,21 +2,23 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'home', pathMatch: 'full'},
-    {path: 'home', 
-        component: Home, 
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {
+        path: 'home',
+        component: Home,
         children: [
-            {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-            {path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard)
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+            {
+                path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard)
             },
             {
-                path:'my-accounts', loadComponent: () => import('./pages/my-accounts/my-accounts').then(m => m.MyAccounts)
+                path: 'my-accounts', loadComponent: () => import('./pages/my-accounts/my-accounts').then(m => m.MyAccounts)
             },
             {
-                path:'transfer-funds', loadComponent: () => import('./pages/transfer-funds/transfer-funds').then(m => m.TransferFunds)
+                path: 'transfer-funds', loadComponent: () => import('./pages/transfer-funds/transfer-funds').then(m => m.TransferFunds)
             },
             {
-                path:'transactions', loadComponent: () => import('./pages/transactions/transactions').then(m => m.Transactions)
+                path: 'transactions', loadComponent: () => import('./pages/transactions/transactions').then(m => m.Transactions)
             },
             {
                 path: 'monthly-statements', loadComponent: () => import('./pages/monthly-statements/monthly-statements').then(m => m.MonthlyStatements)
@@ -26,7 +28,19 @@ export const routes: Routes = [
             },
             {
                 path: 'standing-orders', loadComponent: () => import('./pages/standing-orders/standing-orders').then(m => m.StandingOrders)
-            }
+            },
+            {
+                path: 'customer-detail', loadComponent: () => import('./pages/customer-detail/customer-detail').then(m => m.CustomerDetail)
+            },
+            {
+                path: 'customer-profile', loadComponent: () => import('./pages/customer-profile/customer-profile').then(m => m.CustomerProfile)
+            },
+            {
+                path: 'customer-edit', loadComponent: () => import('./pages/customer-edit/customer-edit').then(m => m.CustomerEdit)
+            },
+            {
+                path: 'customer-create', loadComponent: () => import('./pages/customer-create/customer-create').then(m => m.CustomerCreate)
+            },
         ]
     },
 ];
--- VoltioAngularFrontend/src/app/shared-components/button/button.ts
+++ VoltioAngularFrontend/src/app/shared-components/button/button.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './button.html',
   styleUrl: './button.css',
 })
 export class Button {
 
   @Input() label: string = '';
-  @Input() type: 'primary' | 'secondary' = 'secondary'; 
+  @Input() type: 'primary' | 'secondary' = 'secondary';
+  @Input() link: string = '';
 }
